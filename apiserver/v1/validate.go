229ae09cd6318a8a04ac6479e6b2b0cff49dbed8
Ethan <admin@opsdata.cn>
2022-06-06 15:17:09 +0800

    Validate() for Policy added

--- apiserver/v1/validate.go
+++ apiserver/v1/validate.go
@@ -18,7 +18,6 @@ func (u *User) Validate() field.ErrorList {
 }
 
 // ValidateUpdate validates that a user object is valid when update.
-// Like User.Validate but not validate password.
 func (u *User) ValidateUpdate() field.ErrorList {
 	val := validation.NewValidator(u)
 	return val.Validate()
@@ -27,5 +26,11 @@ func (u *User) ValidateUpdate() field.ErrorList {
 // Validate validates that a secret object is valid.
 func (s *Secret) Validate() field.ErrorList {
 	val := validation.NewValidator(s)
+	return val.Validate()
+}
+
+// Validate validates that a policy object is valid.
+func (p *Policy) Validate() field.ErrorList {
+	val := validation.NewValidator(p)
 	return val.Validate()
 }
